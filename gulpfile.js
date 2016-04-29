3b60ebbe3a4fb00606a8ce0b94f7948370d95ed3
James <jhemerson.comercial@gmail.com>
2016-04-29 11:49:50 -0300

    Removing unecessary items

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,10 @@
    ================================== */
 
 var gulp         = require('gulp'),
-    uglify       = require('gulp-uglify'),
-    concat       = require('gulp-concat'),
-    jshint       = require('gulp-jshint'),
-    concatCss    = require('gulp-concat-css'),
     autoprefixer = require('gulp-autoprefixer'),
     sass         = require('gulp-sass'),
-    compass      = require('compass-importer'),
-    rename       = require('gulp-rename'),
     jade         = require('gulp-jade'),
     sourcemaps   = require('gulp-sourcemaps'),
-    sassdoc      = require('sassdoc'),
     browserify   = require('browserify'),
     source       = require('vinyl-source-stream'),
     browserSync  = require('browser-sync').create();
@@ -26,15 +19,13 @@ var config = {
         jade    : './src/*.jade',
         styles  : './src/styles/**/*.{sass,scss}',
         scripts : './src/scripts/*.js',
-        images  : './src/images/**/*.*',
-        vendor  : './src/vendor/**/*'
+        images  : './src/images/**/*.*'
     },
     dest : {
         html   : './public/',
         css    : './public/css/',
         js     : './public/js/',
         img    : './public/img/',
-        vendor : './public/vendor/',
         maps   : './public/css/maps/'
     }
 },
@@ -58,10 +49,10 @@ gulp.task( 'scripts', function() {
 /* Styles */
 gulp.task( 'styles', function() {
     gulp.src( './src/styles/main.sass' )
-        /*.pipe(sourcemaps.init())*/
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        /*.pipe(sourcemaps.write(config.dest.maps))*/
-        .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
+        .pipe(sourcemaps.init())
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
+        .pipe(sourcemaps.write(config.dest.maps))
         .pipe(gulp.dest(config.dest.css))
         .pipe(browserSync.stream());
 });
@@ -82,11 +73,6 @@ gulp.task( 'images', function() {
         .pipe( gulp.dest( config.dest.img ) );
 });
 
-gulp.task( 'vendor', function() {
-    gulp.src( config.src.vendor )
-        .pipe( gulp.dest(config.dest.vendor) );
-});
-
 gulp.task('serve', ['styles', 'jade', 'scripts'], function() {
 
     browserSync.init({
@@ -94,26 +80,9 @@ gulp.task('serve', ['styles', 'jade', 'scripts'], function() {
         port: 7000
     });
 
-    gulp.watch(config.src.styles, ['styles']);
     gulp.watch(config.src.jade, ['jade']);
+    gulp.watch(config.src.styles, ['styles']);
     gulp.watch(config.src.scripts, ['scripts']);
 });
 
-/* ==================================
-        "To Production" Tasks
-   ================================== */
-
-gulp.task('prod', ['sassdoc'], function () {
-    gulp.src(config.src.styles)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
-        .pipe(gulp.dest(config.dest.css));
-});
-
-gulp.task('watch', function() {
-  gulp.watch([gulp.src.styles], ['styles']);
-  gulp.watch([gulp.src.scripts], ['scripts']);
-  gulp.watch([gulp.src.jade], ['jade']);
-});
-
-gulp.task('default', ['jade', 'scripts', 'styles', 'images', 'vendor', 'serve']);
+gulp.task('default', ['jade', 'scripts', 'styles', 'images', 'serve']);
